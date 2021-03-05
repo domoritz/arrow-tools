4dcfc2a6be0f561bf4f8d1a8158fbc238249d5cf
Dominik Moritz <domoritz@gmail.com>
2021-03-05 17:58:55 +0100

    initial import

---
+++ Readme.md
@@ -0,0 +1,72 @@
+# CSV to Parquet
+
+[![Crates.io](https://img.shields.io/crates/v/csv2parquet.svg)](https://crates.io/crates/csv2parquet)
+[![Rust](https://github.com/domoritz/csv2parquet/actions/workflows/rust.yml/badge.svg)](https://github.com/domoritz/csv2parquet/actions/workflows/rust.yml)
+
+Convert CSV files to Apache Parquet.
+
+## Installation
+
+### Download prebuilt binaries
+
+You can get the latest releases from https://github.com/domoritz/csv2parquet/releases/.
+
+### With Cargo
+
+```
+cargo install csv2parquet
+```
+
+## Usage
+
+```
+USAGE:
+    csv2parquet [FLAGS] [OPTIONS] <CSV> [PARQUET]
+
+ARGS:
+    <CSV>        Input CSV file
+    <PARQUET>    Output file, stdout if not present
+
+FLAGS:
+        --dictionary      Sets flag to enable/disable dictionary encoding for any column
+    -n, --dry             Only print the schema
+        --help            Prints help information
+    -p, --print-schema    Print the schema to stderr
+        --statistics      Sets flag to enable/disable statistics for any column
+    -V, --version         Prints version information
+
+OPTIONS:
+    -c, --compression <compression>
+            Set the compression [possible values: uncompressed, snappy, gzip, lzo, brotli, lz4,
+            zstd]
+
+        --created-by <created-by>                                  Sets "created by" property
+        --data-pagesize-limit <data-pagesize-limit>                Sets data page size limit
+    -d, --delimiter <delimiter>
+            Set the CSV file's column delimiter as a byte character [default: ,]
+
+        --dictionary-pagesize-limit <dictionary-pagesize-limit>    Sets dictionary page size limit
+    -e, --encoding <encoding>
+            Sets encoding for any column [possible values: plain, rle, bit-packed, delta-binary-
+            packed, delta-length-byte-array, delta-byte-array, rle-dictionary]
+
+    -h, --header <header>
+            Set whether the CSV file has headers
+
+        --max-read-records <max-read-records>
+            The number of records to infer the schema from. All rows if not present
+
+        --max-row-group-size <max-row-group-size>                  Sets max size for a row group
+        --max-statistics-size <max-statistics-size>
+            Sets max statistics size for any column. Applicable only if statistics are enabled
+
+        --write-batch-size <write-batch-size>                      Sets write batch size
+```
+
+## For Developers
+
+To format the code, run
+
+```bash
+cargo clippy && cargo fmt
+```
---
+++ src/main.rs
@@ -0,0 +1,202 @@
+use arrow::csv::ReaderBuilder;
+use clap::{Clap, ValueHint};
+use parquet::{
+    arrow::ArrowWriter,
+    basic::{Compression, Encoding},
+    errors::ParquetError,
+    file::properties::WriterProperties,
+};
+use serde_json::to_string_pretty;
+use std::path::PathBuf;
+use std::{fs::File, usize};
+
+#[derive(Clap)]
+enum ParquetCompression {
+    UNCOMPRESSED,
+    SNAPPY,
+    GZIP,
+    LZO,
+    BROTLI,
+    LZ4,
+    ZSTD,
+}
+
+#[derive(Clap)]
+#[allow(non_camel_case_types)]
+enum ParquetEncoding {
+    PLAIN,
+    RLE,
+    BIT_PACKED,
+    DELTA_BINARY_PACKED,
+    DELTA_LENGTH_BYTE_ARRAY,
+    DELTA_BYTE_ARRAY,
+    RLE_DICTIONARY,
+}
+
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Dominik Moritz <domoritz@gmail.com>")]
+struct Opts {
+    /// Input CSV file.
+    #[clap(name = "CSV", parse(from_os_str), value_hint = ValueHint::AnyPath)]
+    input: PathBuf,
+
+    /// Output file, stdout if not present.
+    #[clap(name = "PARQUET", parse(from_os_str), value_hint = ValueHint::AnyPath)]
+    output: Option<PathBuf>,
+
+    /// The number of records to infer the schema from. All rows if not present.
+    #[clap(long)]
+    max_read_records: Option<usize>,
+
+    /// Set whether the CSV file has headers
+    #[clap(short, long)]
+    header: Option<bool>,
+
+    /// Set the CSV file's column delimiter as a byte character.
+    #[clap(short, long, default_value = ",")]
+    delimiter: char,
+
+    /// Set the compression.
+    #[clap(short, long, arg_enum)]
+    compression: Option<ParquetCompression>,
+
+    /// Sets encoding for any column.
+    #[clap(short, long, arg_enum)]
+    encoding: Option<ParquetEncoding>,
+
+    /// Sets data page size limit.
+    #[clap(long)]
+    data_pagesize_limit: Option<usize>,
+
+    /// Sets dictionary page size limit.
+    #[clap(long)]
+    dictionary_pagesize_limit: Option<usize>,
+
+    /// Sets write batch size.
+    #[clap(long)]
+    write_batch_size: Option<usize>,
+
+    /// Sets max size for a row group.
+    #[clap(long)]
+    max_row_group_size: Option<usize>,
+
+    /// Sets "created by" property.
+    #[clap(long)]
+    created_by: Option<String>,
+
+    /// Sets flag to enable/disable dictionary encoding for any column.
+    #[clap(long)]
+    dictionary: bool,
+
+    /// Sets flag to enable/disable statistics for any column.
+    #[clap(long)]
+    statistics: bool,
+
+    /// Sets max statistics size for any column. Applicable only if statistics are enabled.
+    #[clap(long)]
+    max_statistics_size: Option<usize>,
+
+    /// Print the schema to stderr.
+    #[clap(short, long)]
+    print_schema: bool,
+
+    /// Only print the schema
+    #[clap(short = 'n', long)]
+    dry: bool,
+}
+
+fn main() -> Result<(), ParquetError> {
+    let opts: Opts = Opts::parse();
+
+    let input = File::open(opts.input)?;
+
+    let mut builder = ReaderBuilder::new()
+        .has_header(opts.header.unwrap_or(true))
+        .with_delimiter(opts.delimiter as u8);
+    builder = builder.infer_schema(opts.max_read_records);
+
+    let reader = builder.build(input)?;
+
+    if opts.print_schema || opts.dry {
+        let json: String = to_string_pretty(&reader.schema().to_json()).unwrap();
+        eprintln!("Inferred Schema:\n{}", json);
+
+        if opts.dry {
+            return Ok(());
+        }
+    }
+
+    let path = opts.output.unwrap();
+    let output = File::create(&path).unwrap();
+
+    let mut props = WriterProperties::builder()
+        .set_dictionary_enabled(opts.dictionary)
+        .set_statistics_enabled(opts.statistics);
+
+    if let Some(compression) = opts.compression {
+        let compression = match compression {
+            ParquetCompression::UNCOMPRESSED => Compression::UNCOMPRESSED,
+            ParquetCompression::SNAPPY => Compression::SNAPPY,
+            ParquetCompression::GZIP => Compression::GZIP,
+            ParquetCompression::LZO => Compression::LZO,
+            ParquetCompression::BROTLI => Compression::BROTLI,
+            ParquetCompression::LZ4 => Compression::LZ4,
+            ParquetCompression::ZSTD => Compression::ZSTD,
+        };
+
+        props = props.set_compression(compression);
+    }
+
+    if let Some(encoding) = opts.encoding {
+        let encoding = match encoding {
+            ParquetEncoding::PLAIN => Encoding::PLAIN,
+            ParquetEncoding::RLE => Encoding::RLE,
+            ParquetEncoding::BIT_PACKED => Encoding::BIT_PACKED,
+            ParquetEncoding::DELTA_BINARY_PACKED => Encoding::DELTA_BINARY_PACKED,
+            ParquetEncoding::DELTA_LENGTH_BYTE_ARRAY => Encoding::DELTA_LENGTH_BYTE_ARRAY,
+            ParquetEncoding::DELTA_BYTE_ARRAY => Encoding::DELTA_BYTE_ARRAY,
+            ParquetEncoding::RLE_DICTIONARY => Encoding::RLE_DICTIONARY,
+        };
+
+        props = props.set_encoding(encoding);
+    }
+
+    if let Some(size) = opts.write_batch_size {
+        props = props.set_write_batch_size(size);
+    }
+
+    if let Some(size) = opts.data_pagesize_limit {
+        props = props.set_data_pagesize_limit(size);
+    }
+
+    if let Some(size) = opts.dictionary_pagesize_limit {
+        props = props.set_dictionary_pagesize_limit(size);
+    }
+
+    if let Some(size) = opts.dictionary_pagesize_limit {
+        props = props.set_dictionary_pagesize_limit(size);
+    }
+
+    if let Some(size) = opts.max_row_group_size {
+        props = props.set_max_row_group_size(size);
+    }
+
+    if let Some(created_by) = opts.created_by {
+        props = props.set_created_by(created_by);
+    }
+
+    if let Some(size) = opts.max_statistics_size {
+        props = props.set_max_statistics_size(size);
+    }
+
+    let mut writer = ArrowWriter::try_new(output, reader.schema(), Some(props.build()))?;
+
+    for batch in reader {
+        match batch {
+            Ok(batch) => writer.write(&batch)?,
+            Err(error) => return Err(error.into()),
+        }
+    }
+
+    Ok(())
+}
