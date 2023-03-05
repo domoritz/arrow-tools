9861e9f37375b0754ab6339497d4a5f7fd12d373
Lukas Hermann <lukashermann28@gmail.com>
2023-03-05 05:58:11 -0800

    chore: Add `arrow-tools` crate for shared logic between utils (#12)

---
+++ crates/arrow-tools/Readme.md
@@ -0,0 +1,2 @@
+# Arrow-tools
+This crate serves a general util library to go along with all of the crates within the arrow-tools suite.
---
+++ crates/arrow-tools/src/lib.rs
@@ -0,0 +1,3 @@
+//! # Arrow-tools
+//! This crate serves a general util library to go along
+//! with all of the crates within the arrow-tools suite.
